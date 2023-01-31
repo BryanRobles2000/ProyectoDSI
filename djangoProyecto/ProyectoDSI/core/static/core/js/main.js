f55bf4e8ea7510466115979497d74495b05839be
BryanRobles2000 <124021435+BryanRobles2000@users.noreply.github.com>
2023-01-31 01:14:52 -0500

    Delete djangoProyecto directory

--- djangoProyecto/ProyectoDSI/core/static/core/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-});
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.noticias', { delay: 500});
